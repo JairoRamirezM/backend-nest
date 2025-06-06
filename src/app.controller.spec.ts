8bdc345c3bcb946e65a0b78daa8bfd6a12ce46eb
Jairo Ramirez <jairo.ramirezm@agibiz.cl>
2025-06-06 15:13:03 -0400

    Correcciones con npm Pruebas Unitarias (npm run test)

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -15,8 +15,8 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return "Saludo en Español: Hola !"', () => {
+      expect(appController.getSaludoEspañol()).toBe('Saludo en Español: Hola !');
     });
   });
 });
